2063657f8fb9a3c7258ab29cdf29b6e833a82e7f
Binay <binaydas1123@gmail.com>
2025-03-31 13:06:04 +0530

    Home pgae

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -13,50 +13,69 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
 import Link from "next/link";
 import { useState } from "react";
-import {Plus} from "lucide-react";
+import { LoaderCircle, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function () {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const router = useRouter();
 
   return (
     <div className="min-h-screen flex justify-center items-center px-16 pt-24">
       <div className="w-full flex flex-col items-center gap-8">
-      <Card className="md:w-2/3 lg:w-1/3 w-full">
-        <CardHeader>
-          <CardTitle className="text-3xl font-bold">Join using code</CardTitle>
-          <CardDescription>Want to join a room? Enter the code</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="w-full items-center gap-4">
-              <Label htmlFor="code">Room Code</Label>
-              <div className="flex gap-2">
-              <Input
-                id="code"
-                placeholder="xyz-xyz-xyz"
-                type="text"
-                name="code"
-                required
-              />
-              <Button type="submit" className="" disabled={loading}>
-                {loading ? "Joining..." : "Join"}
-              </Button>
+        <Card className="md:w-2/3 lg:w-1/3 w-full">
+          <CardHeader>
+            <CardTitle className="text-3xl font-bold">
+              Join using code
+            </CardTitle>
+            <CardDescription>
+              Want to join a room? Enter the code
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="w-full items-center gap-4">
+                <Label htmlFor="code">Room Code</Label>
+                <div className="flex gap-2">
+                  <Input
+                    id="code"
+                    placeholder="xyz-xyz-xyz"
+                    type="text"
+                    name="code"
+                    required
+                  />
+                  <Button type="submit" className="" disabled={loading}>
+                    {loading ? "Joining..." : "Join"}
+                  </Button>
+                </div>
               </div>
-            </div>
-            {error && <p className="text-red-500 text-sm">{error}</p>}
-          </form>
-        </CardContent>
-      </Card>
-      <Card className="md:w-2/3 lg:w-1/3 w-full">
-        <CardHeader>
-          <CardTitle className="text-3xl font-bold">Create a new room</CardTitle>
-          <CardDescription>Create rooms seamlessly</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Button variant={"outline"} className="w-full"><Plus /> New Room</Button>
-        </CardContent>
-      </Card>
+              {error && <p className="text-red-500 text-sm">{error}</p>}
+            </form>
+          </CardContent>
+        </Card>
+        <Card className="md:w-2/3 lg:w-1/3 w-full">
+          <CardHeader>
+            <CardTitle className="text-3xl font-bold">
+              Create a new room
+            </CardTitle>
+            <CardDescription>Create rooms seamlessly</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Button variant={"outline"} className="w-full" onClick={() => router.replace("/rooms/create")} disabled={loading}>
+              {loading ? (
+                <>
+                  <LoaderCircle className="animate-spin" /> "Creating"
+                </>
+              ) : (
+                <>
+                  <Plus /> New Room
+                </>
+              )}
+            </Button>
+            {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
