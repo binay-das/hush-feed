fb26c930f54a7d0e693ac58bd0e5fcd63955a4c8
Binay <binaydas1123@gmail.com>
2025-04-01 10:29:25 +0530

    Thank you page

---
+++ app/rooms/[roomId]/thank-you/page.tsx
@@ -0,0 +1,7 @@
+const ThankYouPage = () => (
+  <div className="max-w-xl mx-auto text-center pt-24">
+    <h1 className="text-2xl font-bold">Thank you for your responses!</h1>
+    <p className="mt-4">Your feedback has been submitted successfully.</p>
+  </div>
+);
+export default ThankYouPage;
